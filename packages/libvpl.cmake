0ef77fdc8e67c72bd3cd935518487a928d21a08f
lampon <lampon@users.noreply.github.com>
2026-09-10 20:27:52 +0800

    build: pin libvpl to 674d015b (2026-07-06)
    libvpl main (after 2026-08-11) breaks mingw build: mfx_dispatcher_defs.h
    defines its own wcscpy_s macro which conflicts with stralign.h in the
    toolchain headers (mfx_load_dll.cpp: 3 compile errors).

--- packages/libvpl.cmake
+++ packages/libvpl.cmake
@@ -2,8 +2,7 @@ ExternalProject_Add(libvpl
     GIT_REPOSITORY https://github.com/intel/libvpl.git
     SOURCE_DIR ${SOURCE_LOCATION}
     GIT_CLONE_FLAGS "--filter=tree:0"
-    GIT_REMOTE_NAME origin
-    GIT_TAG main
+    GIT_TAG 674d015bcb29
     UPDATE_COMMAND ""
     CONFIGURE_COMMAND ${EXEC} CONF=1 cmake -H<SOURCE_DIR> -B<BINARY_DIR>
         -G Ninja
