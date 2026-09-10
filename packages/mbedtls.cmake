0ad214363e7776f7ac038d51e2ec4b951aaeac31
lampon <lampon@users.noreply.github.com>
2026-09-10 20:30:11 +0800

    build: pin mbedtls to 346ece94 (2026-08-06)
    mbedtls tracks master with a patch (include-cpuid); pin to the commit
    predating Predidit's last successful build to avoid patch drift.

--- packages/mbedtls.cmake
+++ packages/mbedtls.cmake
@@ -4,8 +4,7 @@ ExternalProject_Add(mbedtls
     GIT_CLONE_FLAGS "--filter=tree:0"
     PATCH_COMMAND ${EXEC} git am --3way ${CMAKE_CURRENT_SOURCE_DIR}/mbedtls-*.patch
     UPDATE_COMMAND ""
-    GIT_REMOTE_NAME origin
-    GIT_TAG master
+    GIT_TAG 346ece94ccf5
     GIT_RESET 1ec69067fa1351427f904362c1221b31538c8b57 # v3.5.0
     CONFIGURE_COMMAND ${EXEC} CONF=1 cmake -H<SOURCE_DIR> -B<BINARY_DIR>
         -G Ninja
